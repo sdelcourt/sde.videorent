01c0ac84d654a724178811a2b840ce4e7f2bb8df
sdelcourt <simon.delcourt@imio.be>
2022-03-08 22:32:09 +0100

    Test app setup.

--- src/sde/videorent/tests/test_setup.py
+++ src/sde/videorent/tests/test_setup.py
@@ -17,14 +17,25 @@ class TestInstall(IntegrationTestCase):
         """Test if sde.videorent is installed with portal_quickinstaller."""
         self.assertTrue(self.installer.isProductInstalled('sde.videorent'))
 
+    def test_application_folders(self):
+        """
+        Test if application folders are created correctly.
+        """
+        expected_result = [
+            {'id': 'rentals', 'title': 'Locations', 'allowed_types': ('Rental',)},
+            {'id': 'films',  'title': 'Fiches film', 'allowed_types': ('Film',)},
+            {'id': 'copies',  'title': 'Exemplaires', 'allowed_types': ('VideoCopy',)},
+            {'id': 'customers',  'title': 'Clients', 'allowed_types': ('Customer',)},
+        ]
+        app_folders = self.portal.objectValues('ATFolder')
+        self.assertEquals(len(app_folders), 4)
+        for index, folder in enumerate(app_folders):
+            expected = expected_result[index]
+            self.assertEquals(folder.id, expected['id'])
+            self.assertEquals(folder.Title(), expected['title'])
+            self.assertEquals(folder.immediatelyAddableTypes, expected['allowed_types'])
+
     def test_uninstall(self):
         """Test if sde.videorent is cleanly uninstalled."""
         self.installer.uninstallProducts(['sde.videorent'])
         self.assertFalse(self.installer.isProductInstalled('sde.videorent'))
-
-    # browserlayer.xml
-    def test_browserlayer(self):
-        """Test that ISdeVideorentLayer is registered."""
-        from sde.videorent.interfaces import ISdeVideorentLayer
-        from plone.browserlayer import utils
-        self.assertIn(ISdeVideorentLayer, utils.registered_layers())
