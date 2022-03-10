5e369ce8e42afe71abbf5f10af41e4e08faea5c4
sdelcourt <simon.delcourt@imio.be>
2022-03-10 08:07:23 +0100

    Add zope event to update customer bonus point after a rental creation.

---
+++ src/sde/videorent/events/rental_events.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+def update_customer_bonus_points(rental, event):
+    """
+    When creating a new rental, update the customer bonus points
+    accordingly.
+    """
+    customer = rental.get_customer()
+    if customer:
+        customer.bonus_points += rental.bonus_points()
+        customer.reindexObject()
